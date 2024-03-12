601d6f15743950eabddbd01fa79bc2b4e158468f
vishal1242 <vsinghal28@gmail.com>
2024-03-12 12:23:03 +0530

    My first selenium Code

---
+++ src/test/java/Testclass/BrowserLaunch.java
@@ -0,0 +1,19 @@
+package Testclass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserLaunch {
+ public static void main(String[] args) throws InterruptedException {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver =new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+	driver.get("https://google.com");
+	
+  }
+}
